8e34c65003fb3db761d551d3659737ee76daa119
Troy Coffindaffer <troy.coffindaffer@gmail.com>
2021-10-13 08:43:59 -0700

    Clean up code in App.jsx

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,27 +1,30 @@
 import * as React from "react";
 
 const App = () => {
-  fetch("https://healthruwords.p.rapidapi.com/v1/quotes/?t=Wisdom&maxR=1&size=medium", {
-    "method": "GET",
-    "headers": {
-      "x-rapidapi-host": "healthruwords.p.rapidapi.com",
-      "x-rapidapi-key": process.env.API_KEY
-    }
-  })
-  .then(response => {
-    return response.json();
-  })
-  .then(response => {
-    console.log(response);
-    document.querySelector('img').src = response[0].media;
-  })
-  .catch(err => {
-    console.error(err);
-  });
+  const handleClick = () => {
+    fetch("https://healthruwords.p.rapidapi.com/v1/quotes/?t=Wisdom&maxR=1&size=medium", {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-host": "healthruwords.p.rapidapi.com",
+        "x-rapidapi-key": process.env.API_KEY
+      }
+    })
+    .then(response => {
+      return response.json();
+    })
+    .then(response => {
+      console.log(response);
+      document.querySelector('img').src = response[0].media;
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  };
+  
   return (
     <main>
-      <h1>Hello World!</h1>
       <img />
+      <button onClick={handleClick}>Get Quote!</button>
     </main>
   );
 };

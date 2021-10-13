73adcad198616a41e6a7891905e3eed0d78ff2b5
Troy Coffindaffer <troy.coffindaffer@gmail.com>
2021-10-13 08:53:00 -0700

    Put handleClick into its own module

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,26 +1,9 @@
 import * as React from "react";
+import handleClick from "./handleClick";
 
 const App = () => {
-  const handleClick = () => {
-    fetch("https://healthruwords.p.rapidapi.com/v1/quotes/?t=Wisdom&maxR=1&size=medium", {
-      "method": "GET",
-      "headers": {
-        "x-rapidapi-host": "healthruwords.p.rapidapi.com",
-        "x-rapidapi-key": process.env.API_KEY
-      }
-    })
-    .then(response => {
-      return response.json();
-    })
-    .then(response => {
-      console.log(response);
-      document.querySelector('img').src = response[0].media;
-    })
-    .catch(err => {
-      console.error(err);
-    });
-  };
-  
+  handleClick();
+
   return (
     <main>
       <img />
